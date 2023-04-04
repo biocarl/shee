296c637636978f9b5da218d2777cc1ca6220f34d
Ru79jel <vera-josefine.moultrie@brightstraining.com>
2023-04-04 15:37:18 +0200

    refactor: show what a participant voted for

--- src/app/participant.service.ts
+++ src/app/participant.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParticipantService {
-  private participantName : string = "Anonym";
+  private participantName: string = "Unknown";
 
-  getParticipantName() : string{
+  getParticipantName(): string {
     return this.participantName;
   }
 
-  setParticipantName(name:string){
+  setParticipantName(name: string) {
     this.participantName = name;
   }
 }
