582df549c61a1d071c9a936dcaebbb01b6a0c1f7
ChristianLange <christian.lange@brightstraining.com>
2023-04-05 16:17:32 +0200

    feat: -add qr-code-service -add functionality to add QR-codes with the actual roomName -add QR-code to our poll-presenter html template

---
+++ src/app/qr-code.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import * as QRCode from 'qrcode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QrCodeService {
+
+  generateQrCode(input: string): Promise<string> {
+    return QRCode.toDataURL(input);
+  }
+
+}
