d6da44a21d05e58c96057f1d594e632d178d4ff3
Sebastian Offergeld <sebastian.offergeld@mailbox.org>
2023-04-12 17:07:12 +0200

    Add imports and sendIdea functionality to brainstorming-client.component.ts

--- src/app/brainstorming/brainstorming-client/brainstorming-client.component.ts
+++ src/app/brainstorming/brainstorming-client/brainstorming-client.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import {ClientView} from "../../client-view";
+import {PresenterMessage} from "../../presenter-message";
+import {BrainstormingPresenterSubscribeResponse} from "../brainstorming-presenter-subscribe-response";
+import {GroupService} from "../../group.service";
+import {QueueService} from "../../queue.service";
+import {ParticipantService} from "../../participant.service";
+import {BrainstormingClientPublishRequest} from "../brainstorming-client-publish-request";
+
 
 @Component({
   selector: 'app-brainstorming-client',
   templateUrl: './brainstorming-client.component.html',
   styleUrls: ['./brainstorming-client.component.css']
 })
-export class BrainstormingClientComponent {
+export class BrainstormingClientComponent implements ClientView{
+  ideaEvent ?: BrainstormingPresenterSubscribeResponse;
+  openForIdeas: boolean = true;
+
+  constructor(private groupService: GroupService, private queueService: QueueService, private participantService: ParticipantService) {
+  }
+
+  sendIdea() {
+    let idea_text: string = "";
+    if (!this.ideaEvent?.idea_id) return
+    const idea: BrainstormingClientPublishRequest = {
+      interaction: "brainstorming",
+      idea_id : this.ideaEvent.idea_id,
+      idea_text: idea_text,
+      participantName: this.participantService.getParticipantName()
+    };
+    this.queueService.publishMessageToClientChannel<BrainstormingClientPublishRequest>(idea);
+  }
+
+  initializeComponent(data: PresenterMessage) {
+    this.ideaEvent = data as BrainstormingPresenterSubscribeResponse;
+  }
 
 }
