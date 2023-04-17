b41e356bb20c267ba5c4909fbb633098b2e74bd6
Ru79jel <vera-josefine.moultrie@brightstraining.com>
2023-04-17 10:01:52 +0200

    feat: show on clientside that a message was sent

--- src/app/brainstorming/brainstorming-client/brainstorming-client.component.ts
+++ src/app/brainstorming/brainstorming-client/brainstorming-client.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ClientView} from "../../client-view";
 import {PresenterMessage} from "../../presenter-message";
 import {BrainstormingPresenterSubscribeResponse} from "../brainstorming-presenter-subscribe-response";
@@ -13,10 +13,11 @@ import {BrainstormingClientPublishRequest} from "../brainstorming-client-publish
   templateUrl: './brainstorming-client.component.html',
   styleUrls: ['./brainstorming-client.component.css']
 })
-export class BrainstormingClientComponent implements ClientView{
+export class BrainstormingClientComponent implements ClientView {
   ideaEvent ?: BrainstormingPresenterSubscribeResponse;
   openForIdeas: boolean = true;
   idea_text: string = "";
+  is_sent: boolean = false;
 
   constructor(private groupService: GroupService, private queueService: QueueService, private participantService: ParticipantService) {
   }
@@ -24,7 +25,6 @@ export class BrainstormingClientComponent implements ClientView{
   sendIdea() {
 
     if (!this.ideaEvent?.question_id) return
-
 
     const idea: BrainstormingClientPublishRequest = {
       interaction: "brainstorming",
@@ -34,11 +34,17 @@ export class BrainstormingClientComponent implements ClientView{
     };
 
     this.queueService.publishMessageToClientChannel<BrainstormingClientPublishRequest>(idea);
+
+    this.idea_text = "";
+    this.is_sent = true;
+
+    setTimeout(() => {
+      this.is_sent = false
+    }, 1000)
   }
 
   initializeComponent(data: PresenterMessage) {
 
     this.ideaEvent = data as BrainstormingPresenterSubscribeResponse;
   }
-
 }
