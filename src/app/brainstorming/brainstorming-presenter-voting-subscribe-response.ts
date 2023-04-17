358d72236d7cdf5f4a040585e6399f6d0e87126a
Sebastian Offergeld <sebastian.offergeld@mailbox.org>
2023-04-17 16:49:36 +0200

    Change interface for brainstorming voting functionality

--- src/app/brainstorming/brainstorming-presenter-voting-subscribe-response.ts
+++ src/app/brainstorming/brainstorming-presenter-voting-subscribe-response.ts
@@ -1,6 +1,7 @@
 export interface BrainstormingPresenterVotingSubscribeResponse {
   question_id: string;
   single_choice: boolean;
+  voting_in_progress: boolean;
   interaction: "brainstorming",
   ideas: string[],
   question: string;
