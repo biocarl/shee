48eeef75b451ab6e3addeb6ca1c12f62215cf392
Sebastian B <123449257+Bohdoh@users.noreply.github.com>
2023-04-18 13:16:01 +0200

    Source Code Documentation JSDoc (#33)
    * feat: jsdoc for almost all files 🤮
    
    * feat: jsdoc for almost all files 🤮
    
    * feat: documentation for queue.service.ts
    
    * feat: jsdoc for  all files done 🙃🔫
    
    * fix: random string -> predefined string
    
    * VAG-X: Remove verbose docstrings and add more context - Part 1
    
    * VAG-X: Remove verbose docstrings and add more context - Part 2
    
    ---------
    
    Co-authored-by: Ru79jel <vera-josefine.moultrie@brightstraining.com>
    Co-authored-by: biocarl <bioflash1@web.de>

--- src/app/client-view.ts
+++ src/app/client-view.ts
@@ -1,5 +1,14 @@
 import {PresenterMessage} from "./presenter-message";
 
+/**
+ * The ClientView interface defines the contract for a component that can display a presenter message to a client.
+ * @interface
+ */
 export interface ClientView {
+  /**
+   * Initializes the component with data from a presenter message.
+   * @param {PresenterMessage} data The data from the presenter message to display.
+   * @returns {void}
+   */
   initializeComponent(data : PresenterMessage) : void;
 }
