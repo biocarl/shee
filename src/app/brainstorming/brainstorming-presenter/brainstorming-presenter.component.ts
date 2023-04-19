710eb4eac0a519dac777b615f98816f17ff3b89a
Ru79jel <63743826+Ru79jel@users.noreply.github.com>
2023-04-19 09:58:52 +0200

    Revert "VAG-13 (#44)" (#45)
    This reverts commit 56365d03375e3bcee24a4bfcb79844a80c976e6f.

--- src/app/brainstorming/brainstorming-presenter/brainstorming-presenter.component.ts
+++ src/app/brainstorming/brainstorming-presenter/brainstorming-presenter.component.ts
@@ -1,10 +1,9 @@
-import {AfterViewChecked, Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PresenterView} from "../../presenter-view";
 import {PresenterMessage} from "../../presenter-message";
 import {BrainstormingPresenterSubscribeResponse} from "../brainstorming-presenter-subscribe-response";
 import {QueueService} from "../../queue.service";
 import {BrainstormingClientSubscribeResponse} from "../brainstorming-client-subscribe-response";
-import {CdkDragStart} from '@angular/cdk/drag-drop';
 
 
 @Component({
@@ -12,21 +11,11 @@ import {CdkDragStart} from '@angular/cdk/drag-drop';
   templateUrl: './brainstorming-presenter.component.html',
   styleUrls: ['./brainstorming-presenter.component.css']
 })
-export class BrainstormingPresenterComponent implements PresenterView, OnInit,AfterViewChecked {
+export class BrainstormingPresenterComponent implements PresenterView, OnInit {
   ideaEvent ?: BrainstormingPresenterSubscribeResponse;
   ideaResponses : string[] = [];
-  maxZIndex = 20;
-  chosenColor: string = "#FFD707FF";
 
   constructor(private queueService: QueueService) {
-  }
-
-  ngAfterViewChecked(): void {
-    const stickies = document.querySelectorAll('.sticky');
-
-    stickies.forEach(sticky => {
-      this.resizeTextToFitContainer(sticky as HTMLElement);
-    });
   }
 
   ngOnInit(): void {
@@ -38,6 +27,7 @@ export class BrainstormingPresenterComponent implements PresenterView, OnInit,Af
       if (this.ideaEvent.question_id == brainstormingSubscriptionEvent.question_id) {
         this.ideaResponses.push(brainstormingSubscriptionEvent.idea_text);
       }
+
     });
   }
 
@@ -50,75 +40,4 @@ export class BrainstormingPresenterComponent implements PresenterView, OnInit,Af
     */
   }
 
-  resizeTextToFitContainer(element: HTMLElement) {
-    const maxWidth = element.clientWidth;
-    const maxHeight = element.clientHeight;
-
-    let minFontSize = 5; // Set a minimum font size
-    let maxFontSize = 36; // Set a maximum font size
-    let fontSize = maxFontSize;
-
-    // Apply the maximum font size
-    element.style.fontSize = fontSize + 'px';
-
-    // Reduce the font size until the content fits or reaches the minimum size
-    while ((element.scrollHeight > maxHeight || element.scrollWidth > maxWidth) && fontSize > minFontSize) {
-      fontSize--;
-      element.style.fontSize = fontSize + 'px';
-    }
-
-    // Increase the font size until the content overflows or reaches the maximum size
-    while ((element.scrollHeight <= maxHeight && element.scrollWidth <= maxWidth) && fontSize < maxFontSize) {
-      fontSize++;
-      element.style.fontSize = fontSize + 'px';
-
-      if (element.scrollHeight > maxHeight || element.scrollWidth > maxWidth) {
-        fontSize--;
-        element.style.fontSize = fontSize + 'px';
-        break;
-      }
-    }
-  }
-
-  moveToTopLayerWhenDragged(event: CdkDragStart) {
-    const element = event.source.getRootElement();
-    const stickyElement = element.querySelector('.sticky') as HTMLElement;
-    const shadowElement = element.querySelector('.shadow') as HTMLElement;
-    const postItElement = stickyElement.parentElement;
-    const tapeElement = element.querySelector('.top-tape') as HTMLElement;
-    const iconsElement = element.querySelector('.icon-container') as HTMLElement;
-
-    if (stickyElement && stickyElement.parentElement && postItElement) {
-      postItElement.style.zIndex = (++this.maxZIndex).toString();
-      stickyElement.style.zIndex = (++this.maxZIndex).toString();
-    }
-
-    if (shadowElement) {
-      shadowElement.style.zIndex = (this.maxZIndex - 1).toString();
-    }
-
-    if (tapeElement) {
-      tapeElement.style.zIndex = (this.maxZIndex +1).toString();
-    }
-    if (iconsElement) {
-      iconsElement.style.zIndex = (this.maxZIndex +1).toString();
-    }
-  }
-
-  hideIdea(i:number) {
-    if (i > -1) {
-      this.ideaResponses.splice(i, 1,"");
-    }
-  }
-
-  trackByIndex(index: number): number {
-    return index;
-  }
-
-  showCopiedMessage(element: HTMLElement) {
-    element.style.opacity = '1';
-    setTimeout(() => {
-      element.style.opacity = '0';
-    }, 1200);
-  }
 }
