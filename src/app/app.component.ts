e8299b7f0ab848b81bc111e23a0769e34a2d79fe
Sebastian Tamayo <sebastian-tamayo.pacheco@brightstraining.com>
2023-04-24 15:57:03 +0200

    ft: VAG-10 is merge with the Main 24.04.2023 15:56

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {GroupService} from "./group.service";
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
