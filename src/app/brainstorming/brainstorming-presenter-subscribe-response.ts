6449c8d60d9202eae90d8547324134188e18f19e
Ru79jel <63743826+Ru79jel@users.noreply.github.com>
2023-04-25 10:23:12 +0200

    voting-hotfix (#57)
    * fix: voting
    
    * fix: votes are mapped correctly to stickies
    
    ---------
    
    Co-authored-by: Sebastian Bodo Hennicke <sebastian.hennicke@brightstraining.com>
    Co-authored-by: Ru79jel <vera-josefine.moultrie@brightstraining.com>

--- src/app/brainstorming/brainstorming-presenter-subscribe-response.ts
+++ src/app/brainstorming/brainstorming-presenter-subscribe-response.ts
@@ -1,7 +1,6 @@
 export interface BrainstormingPresenterSubscribeResponse {
   interaction: "brainstorming",
   question_id: string,
-  ideas: string[],
   question: string,
   timer?: number,
   voting_in_progress: boolean,
