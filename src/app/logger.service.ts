2c9189803a99cf06c6ed32c85f903110459b461f
Sebastian Bodo Hennicke <sebastian.hennicke@brightstraining.com>
2023-04-28 11:02:18 +0200

    fix: JSDoc for loggerService

--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -4,10 +4,23 @@ import {environment} from "../environments/environment.development";
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * LoggerService is a utility service for logging messages to the console in a non-production environment.
+ * @class
+ * @Injectable
+ */
 export class LoggerService {
 
   constructor() { }
 
+  /**
+   * Logs a message and an optional object to the console if the application is running in a non-production environment.
+   * If the object has a message property, its decoded content will also be logged.
+   * @param {string} message - The message to log to the console.
+   * @param {any} [Obj] - The optional object to log to the console.
+   * @public
+   * @returns {void}
+   */
   public toConsole(message: string, Obj?:any){
     if(!environment.production){
       const timestamp = `${new Date().toLocaleTimeString()}.${String(new Date().getMilliseconds()).padStart(3, "0")}`;
