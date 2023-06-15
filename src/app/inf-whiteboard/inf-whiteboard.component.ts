98d7e8c3e3f8add9e0c94d5475154f2f1aad4e4c
Sebastian Bodo Hennicke <sebastian.hennicke@brightstraining.com>
2023-06-15 16:39:11 +0200

    feat: stickys can be created and textbox edited. Text still breaks out for now (wip)

--- src/app/inf-whiteboard/inf-whiteboard.component.ts
+++ src/app/inf-whiteboard/inf-whiteboard.component.ts
@@ -1,99 +1,75 @@
-import {AfterViewInit, Component, HostListener} from '@angular/core';
-import {fabric} from 'fabric';
-
+import { Component, OnInit } from '@angular/core';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-inf-whiteboard',
   templateUrl: './inf-whiteboard.component.html',
   styleUrls: ['./inf-whiteboard.component.css']
 })
-export class InfWhiteboardComponent implements AfterViewInit {
+export class InfWhiteboardComponent implements OnInit {
   private canvas!: fabric.Canvas;
-  private isDragging: boolean = false;
-  private lastPosX: number = 0;
-  private lastPosY: number = 0;
-  private selection: boolean = true;
 
-
-
-  ngAfterViewInit() {
+  ngOnInit (): void {
     this.canvas = new fabric.Canvas('canvas', {
-      selection: false,
-      hoverCursor: 'default',
-      backgroundColor: 'lightgray'
+      backgroundColor: 'white',
     });
+    this.canvas.setWidth(window.innerWidth - 100);
+    this.canvas.setHeight(window.innerHeight);
 
-    this.canvas.setWidth(window.innerWidth);
-    this.canvas.setHeight(window.innerHeight-220);
-    this.canvas.renderAll();
-
-    this.canvas.on('mouse:wheel', (opt) => {
-      const delta = opt.e.deltaY;
-      let zoom = this.canvas.getZoom();
-      zoom *= 0.999 ** delta;
-      if (zoom > 20) zoom = 20;
-      if (zoom < 0.01) zoom = 0.01;
-      this.canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
-      opt.e.preventDefault();
-      opt.e.stopPropagation();
-    });
-
-    this.canvas.on('mouse:down', (opt) => {
-      var evt = opt.e;
-      if (evt.altKey) {
-        this.isDragging = true;
-        this.selection = false;
-        this.lastPosX = evt.clientX;
-        this.lastPosY = evt.clientY;
-      }
-    });
-
-
-
-    this.canvas.on('mouse:up', () => {
-      // @ts-ignore
-      this.canvas.setViewportTransform(this.canvas.viewportTransform);
-      this.isDragging = false;
-      this.selection = true;
-    });
-
-
-    this.canvas.on('mouse:move', (opt) => {
-      if (this.isDragging) {
-        var e = opt.e;
-        var vpt = this.canvas.viewportTransform;
-        if (vpt) {
-          vpt[4] += e.clientX - this.lastPosX;
-          vpt[5] += e.clientY - this.lastPosY;
+    this.canvas.on('mouse:dblclick', (options) => {
+      let target = options.target as fabric.Group;
+      if (target && target.type === 'group') {
+        // Get the objects and select the textbox.
+        let items = target.getObjects();
+        let textbox = items.find((obj) => obj.type === 'textbox') as fabric.Textbox;
+        if (textbox) {
+          console.log('Entering editing mode for:', textbox);
+          target.removeWithUpdate(textbox);
+          this.canvas.add(textbox);
+          this.canvas.setActiveObject(textbox);
+          textbox.enterEditing();
+          items.forEach((item) => {
+            if (item !== textbox) {
+              item.set({ selectable: false });
+            }
+          });
         }
-        this.canvas.requestRenderAll();
-        this.lastPosX = e.clientX;
-        this.lastPosY = e.clientY;
       }
     });
   }
 
   addStickyNote() {
-    const text = new fabric.IText('Sticky note!', {
-      fill: '#000000',
-      backgroundColor: '#FFFF00',
-      padding: 10,
-      hasControls: true,
-      lockUniScaling: false,
+    let rectangle = new fabric.Rect({
+      left: 0,
+      top: 0,
+      width: 200,
+      height: 200,
+      fill: 'yellow',
     });
 
-    text.setControlsVisibility({
-
+    let textbox = new fabric.Textbox('Some text', {
+      left: 30,  // Start from the padding size
+      top: 30,   // Start from the padding size
+      width: 140,  // Rectangle's width (200) - 2 * padding size (30)
+      fill: 'black',
+      splitByGrapheme: true,
     });
 
-    this.canvas.add(text);
-  }
+      textbox.on('editing:exited', () => {
+      console.log('Exited editing mode for:', textbox);
 
+      // Re-add the textbox to the group after editing is finished.
+      group.addWithUpdate(textbox);
+      this.canvas.remove(textbox);
 
-  @HostListener('window:resize')
-  onResize() {
-    this.canvas.setWidth(window.innerWidth);
-    this.canvas.setHeight(window.innerHeight);
+      // Instead of removing the group, just deselect it and stop editing.
+      this.canvas.discardActiveObject().renderAll();
+
+      // Make items selectable again
+      group.getObjects().forEach(item => item.set('selectable', true));
+    });
+
+    let group = new fabric.Group([rectangle, textbox], {});
+    this.canvas.add(group);
   }
 }
-
