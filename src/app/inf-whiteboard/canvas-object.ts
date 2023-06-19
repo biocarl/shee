aa258a688c3fe4277ae16cac626c0c90b5ec13e3
Sebastian Bodo Hennicke <sebastian.hennicke@brightstraining.com>
2023-06-19 14:49:19 +0200

    feat: created CanvasObject abstract class so every CanvasObject has to have create and attackDlbClickHandler method

--- src/app/inf-whiteboard/canvas-object.ts
+++ src/app/inf-whiteboard/canvas-object.ts
@@ -3,6 +3,4 @@ import {fabric} from "fabric";
 export abstract class CanvasObject<T> {
   public abstract create():fabric.Object;
   protected abstract attachDoubleClickHandler(object: T):void;
-
-
 }
