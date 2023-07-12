dab9632a60df3ab54defead309d20fc024683096
Ru79jel <vera-josefine.moultrie@brightstraining.com>
2023-07-12 16:10:03 +0200

    style: style landing page wip

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+import {Mode} from "../mode-toggle/mode-toggle.model";
+import {Subscription} from "rxjs";
+import {ModeToggleService} from "../mode-toggle/mode-toggle.service";
 
 @Component({
   selector: 'app-landing-page',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
+  roomName: string = "";
+  mode: Mode;
+  Mode = Mode;
+  modeSubscription: Subscription;
 
+  constructor(private router: Router, private modeToggleService: ModeToggleService,) {
+    this.mode = modeToggleService.currentMode;
+    this.modeSubscription = modeToggleService.modeChanged$.subscribe(
+      (mode: Mode) => {
+        this.mode = mode;
+      }
+    );
+  }
+
+  navigateToInitializer() {
+    this.router.navigate(["../" + this.roomName + "/presenter/new"]).then()
+  }
 }
